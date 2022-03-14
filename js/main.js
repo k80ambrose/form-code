7b42555517f871c6d225333d39303f6b6828293b
k80 <81832771+k80ambrose@users.noreply.github.com>
2022-03-14 18:37:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,3 +6,9 @@ function setup() {
   let r = width / 4
   ellipse(x, y, r)
 }
+osc(4, 0.042, 0.481)
+	.color(1.17, 0.387, -1.1)
+	.rotate(0.30, 0.1)
+	.pixelate(0.076, 20)
+	.modulate(noise(2.5), () => 1.5 * Math.sin(0.08 * time))
+	.out(o0);
