c99bb38305be6c3dd387b276989cd8dcb3b9da6c
k80 <k80@uchicago.edu>
2022-03-17 22:52:32 -0500

    yuhhhh

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
-osc(7.044, 0.042, 0.471)
-	.color(1.17, 0.76, -1.1)
-	.rotate(0.90, 0.124)
-	.pixelate(8.76, 22.063)
-	.modulate(noise(5.09), () => 0.5 * Math.sin(0.08 * time))
-	.out(o0);
+<style>
+  body {
+    margin: 0;
+  }
+</style>
+<body>
+
+  <canvas id="hydra-canvas"></canvas>
+
+  <script src="https://unpkg.com/hydra-synth"></script>
+  <script>
+    /* global Hydra, osc, noise, o0, time */
+
+    const canvas = document.querySelector('#hydra-canvas')
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+
+    new Hydra({
+      canvas: canvas,
+      detectAudio: false,
+      enableStreamCapture: false,
+      width: canvas.width,
+      height: canvas.height
+    })
+
+    // by Olivia Jack
+    // https://ojack.github.io
+    osc(4, 0.042, 0.481)
+      .color(1.17, 0.387, -1.1)
+      .rotate(0.30, 0.1)
+      .pixelate(0.076, 20)
+      .modulate(noise(2.5), () => 1.5 * Math.sin(0.08 * time))
+      .out(o0)
+
+  </script>
+</body>
