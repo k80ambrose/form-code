000d78d67f40d98c349dacb884ea7866b581d8e5
k80 <k80@uchicago.edu>
2022-03-18 03:07:15 -0500

    i am the cs boyfriend i needed help from 2 hours ago

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-const canvas = document.querySelector('#hydra-canvas')
+  const canvas = document.querySelector('#hydra-canvas')
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
-      new Hydra({
-        canvas: canvas,
-        detectAudio: false,
-        enableStreamCapture: false,
-        width: canvas.width,
-        height: canvas.height
-      })
+  new Hydra({
+    canvas: canvas,
+    detectAudio: false,
+    enableStreamCapture: false,
+    width: canvas.width,
+    height: canvas.height
+  })
 
-      var clicks = 0;
-      function onClick () {
-        clicks += 1;
-      }
+  var clicks = 0;
+    function onClick () {
+    clicks += 1;
+  }
 
-      var x = MouseEvent.clientX;
-      var y = MouseEvent.clientY;
+  document.body.onkeyup = function(e){
+      if(e.keyCode == 32){
+        location.reload();
+      }}
 
-      osc(1, 0.42, 0.7)
-        .color((Math.random() - Math.sin(0.03 * time)), (Math.random() + Math.sin(0.08 * time)),(Math.random()))
-        .rotate(0.90, 0.1)
-        .pixelate(4, 4)
-        .modulate(noise(3), () => (1.5 + clicks) * Math.sin(0.03 * time))
-        .out(o0)
+  osc(1, 0.42, 0.7)
+    .color((Math.random() - Math.sin(0.03 * time)), (Math.random() + Math.tan(0.08 * time)),(Math.random()), 0.5)
+    .rotate(0.90, 0.1)
+    .pixelate(4, 4)
+    .modulate(noise(3), () => (1.5 + clicks) * Math.sin(0.025 * time))
+    .out(o0)
 
         // by Olivia Jack
         // https://ojack.github.io
